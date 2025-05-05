753a3fecb85d320ccbd8706a722a11f59e49491b
Mike <mclancy96@gmail.com>
2025-05-05 14:10:05 -0400

    Add new nums and make mod work

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const num1 = 62;
 const num2 = 1;
+const num3 = 14;
+const num4 = 5;
 
 const multiply = num1 * num2;
 
 const random = Math.ceil(Math.random() * 100)
 
-// const mod 
+const mod = 14 % 5;
 
 // const max
